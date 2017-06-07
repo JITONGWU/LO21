1db2ec703ae992c107ee7e76cdd175b738198a00
JITONGWU <wuatong0721@gmail.com>
2017-06-07 18:52:38 +0200

    Update relation.h

--- relation.h
+++ relation.h
@@ -2,6 +2,8 @@
 #define RELATION_H
 #include<QStringList>
 #include "notes.h"
+#include <stdlib.h>
+
 class Couple{
     QString label;
     Note* x;
@@ -13,6 +15,8 @@ public:
     Note* getX() const {return x;}
     Note* getY() const {return y;}
     QString getEtat()const {return etat;}
+    void setX(Note* a){x=a;}
+    void setY(Note* b){y=b;}
     void setLabel(const QString& l){label=l;}
     void setEtat(const QString& e){etat=e;}
     QString AfficherCouple();
@@ -22,50 +26,26 @@ class Relation{
 protected:
     QString titre;
     QString desc;
-    Couple** couples ;
+    QList<Couple*> couples ;
     bool orient;
-    unsigned int nb;
-    unsigned int nbmax;
-
 public:
-    void addCouple(Couple* c);
-    Relation(const QString& t, const QString& d,bool o=true,Couple** c=nullptr,int n=0,int m=0):
-        titre(t),desc(d),couples(c),orient(o),nb(n),nbmax(m){}
+    Relation(const QString& t, const QString& d,bool o=true,QList<Couple*> *c= nullptr):
+        titre(t),desc(d),couples(*c),orient(o){}
+    ~Relation(){couples.clear();}
 
     void addCouple(const QString& lab,Note* x,Note* y,const QString& e);// seulement pour les notes de dernière version!
-    void retirerCouple(QString& label);//
     QString getTitre()const {return titre;}
     QString getDesc()const{return desc;}
-    unsigned int getNb()const {return nb;}
     bool getOrient()const {return orient;}
+    QList<Couple*> getCouples() const {return couples;}
+
+    void setTitre(const QString& t){titre=t;}
+    void setDesc(const QString& d){desc=d;}
+    void setOrient(bool o){orient=o;}
+
+
     Couple* getCouple(const QString& l);//get couple par label
     Couple* getCouple(unsigned int i);//get couple par la position dans le tableau
-
-    class Iterator {
-            friend class Relation;
-            Couple** currentC;
-            unsigned int nbRemain;
-            Iterator(Couple** a, unsigned nb):currentC(a),nbRemain(nb){}
-        public:
-            Iterator():currentC(nullptr),nbRemain(0){}
-            bool isDone() const { return nbRemain==0; }
-            void next() {
-                if (isDone())
-                    throw NotesException("error, next on an iterator which is done");
-                nbRemain--;
-                currentC++;
-            }
-           Couple& current() const {
-                if (isDone())
-                    throw NotesException("error, indirection on an iterator which is done");
-                return **currentC;
-            }
-        };
-        Iterator getIterator() {
-            return Iterator(couples,nb);
-        }
-
-// for(
 };
 
 class Reference:public Relation{  //singleton
@@ -77,27 +57,11 @@ private:
         };
 public:
     Reference():Relation("Référence","\ref{idNote}"){}
-
-
     static Handler handler;
     static Reference& getRef();
     static void freeRef();
 
 };
-/*
-
-void addReference(QString& s){
-    // lier tous les attributs de type string
-    QStringList lst=s.split("\ref{",QString::SkipEmptyParts,Qt::CaseSensitive);  // majuscule  ? minuscule ?
-    for(int i=1;i<lst.count();i++)
-    {
-        QStringList l=lst.at(i).split("}",QString::SkipEmptyParts, Qt::CaseSensitive);
-        std::cout<<l.at(0).trimmed().toStdString()<<std::endl;
-        //if(getNote(l.at(0)) )  //si on trouve les notes avec id= l.at(0)
-        // reference.addCouple(Couple::setCouple(getNote(id),getNote(l.at(0))));
-    }
-
-}*/
 #endif // RELATION_H
 //créer une note --- choisir le type --- remplir les attribues --- sauvegarder ( si on trouve \ref, addCouple()dans reference)
 //choisir une note existant --- modifier / supprimer ce note --- sauvegarder ( si on trouve \ref, addCouple()dans reference)
